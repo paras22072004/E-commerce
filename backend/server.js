e255dc34adf44d30d3a53ea61fa8f8aef24f3f3f
paras22072004 <parasbiltoria22@gmail.com>
2025-08-01 14:02:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const connectDB = require("./db/connect");
-
-// const productRoutes = require("./routes/productRoutes");
-// const userRoutes = require("./routes/userRoutes");
-
-// dotenv.config();
-// const app = express();
-
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/products", productRoutes);
-// app.use("/users", userRoutes);
-// app.use("/uploads", express.static("uploads"));
-
-
-
-
-// connectDB().then(() => {
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log("Server running on port", PORT);
-//   });
-// });
-
 
 const express = require("express");
 const cors = require("cors");
@@ -43,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is live ✅");
+  res.send("Backend is live ");
 });
 
 app.use("/products", productRoutes);

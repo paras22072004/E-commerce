2001bda8be34be92ff5c7eac78f855d77f66780a
Paras Kumar <parasbiltoria22@gmail.com>
2025-08-01 11:34:11 +0530

    Initialcommit

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,9 @@ app.use("/uploads", express.static("uploads"));
 
 
 connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Server running on port", process.env.PORT);
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log("Server running on port", PORT);
   });
 });
 
